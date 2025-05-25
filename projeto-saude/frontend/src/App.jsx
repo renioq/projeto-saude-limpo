0b1e319e5b82cd8ccb50c780084d0cd0b5f981d7
renioq <renioqueiroz@gmail.com>
2025-05-25 12:30:07 -0300

    Ajuste App.jsx 7

--- projeto-saude/frontend/src/App.jsx
+++ projeto-saude/frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Routes, Route, useNavigate, BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header.jsx';
 import Catalog from './pages/Catalog.jsx';
 import Articles from './pages/Articles.jsx';
@@ -7,12 +7,12 @@ import Login from './pages/Login.jsx';
 import MinhasVacinas from './pages/MinhasVacinas.jsx';
 
 const App = () => {
-  //const [logado, setLogado] = useState(!!localStorage.getItem('token'));
+  const [logado, setLogado] = useState(!!localStorage.getItem('token'));
 
 try {
     console.log('App está carregando...');
     return (
-      <BrowserRouter>
+      <>
         <Header />
         <Routes>
           <Route path="/" element={<Catalog />} />
@@ -20,7 +20,7 @@ try {
           <Route path="/login" element={<Login onLoginSuccess={() => setLogado(true)} />} />
           <Route path="/vacinas" element={<MinhasVacinas onLogout={() => setLogado(false)} />} />
         </Routes>
-      </BrowserRouter>
+      </>
     );
   } catch (e) {
     return <p>Erro no componente: {e.message}</p>;
