7169bc7d5498d33751f53768778e10fdc9fa7195
renioq <renioqueiroz@gmail.com>
2025-05-25 16:44:08 -0300

    fix login

--- projeto-saude/backend/index.js
+++ projeto-saude/backend/index.js
@@ -5,12 +5,14 @@ const app = express();
 
 const unitService = require('./services/unitService');
 const articleService = require('./services/articleService');
+const authRoutes = require('./services/auth');
 
 app.use(cors());
 app.use(express.json());
 
 app.use('/api', unitService);
 app.use('/api', articleService);
+app.use('/api', authRoutes);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Backend rodando na porta ${PORT}`));
