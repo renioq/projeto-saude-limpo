7831901196b13d696738b55ef2ea1271e92b0c5d
renioq <renioqueiroz@gmail.com>
2025-05-25 17:01:11 -0300

    FIX CORS 2

--- projeto-saude/backend/index.js
+++ projeto-saude/backend/index.js
@@ -6,13 +6,20 @@ const app = express();
 const unitService = require('./services/unitService');
 const articleService = require('./services/articleService');
 const authRoutes = require('./services/auth');
+const vacineService = require('./services/vacineService');
 
-app.use(cors({ origin: 'https://projeto-saude-vfinal-y3kx.vercel.app' }));
+app.use(cors({ 
+    origin: 'https://projeto-saude-vfinal-y3kx.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+app.options('*', cors()); // habilita resposta para OPTIONS de qualquer rota
 app.use(express.json());
 
 app.use('/api', unitService);
 app.use('/api', articleService);
 app.use('/api', authRoutes);
+app.use('/api', vacineService);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Backend rodando na porta ${PORT}`));
